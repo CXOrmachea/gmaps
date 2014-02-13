31b11ca9e2f649cc8a8f13dc506a69194c0385a7
CXOrmachea <CXOrmachea@gmail.com>
2014-02-13 15:26:09 -0300

    geocoding using yandex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :address, :description, :latitude, :longitude, :title
 
-  geocoded_by :address
+  geocoded_by :address, :lookup => :yandex
   after_validation :geocode
 end
