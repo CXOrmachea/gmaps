3dd3fb67c0e41e58a9ceb8674a60c9682c655014
CXOrmachea <CXOrmachea@gmail.com>
2014-02-14 15:22:58 -0300

    some change to the user model, don't know what

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :address, :description, :latitude, :longitude, :title
 
-  geocoded_by :address, :lookup => :yandex
+  geocoded_by :address, :lookup => :bing 
   after_validation :geocode
 end
